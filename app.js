adb101ec34abed951f5c008543a5c74c9c1cd1b8
surinnnnnn <didthfls2@naver.com>
2024-10-18 18:21:08 +0900

    update:생성, 수정, 삭제 기능 완성

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import cors from "cors";
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
 
-import indexRouter from "./routes/index.js";
-import menusRouter from "./routes/menus.js";
-import menuRankRputer from "./routes/menuRank.js";
+import indexRouter from './routes/index.js';
+import menusRouter from './routes/menus.js';
 
 const app = express();
 
 app.use(cors());
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.static(path.join(path.resolve(), 'public')));
 
-app.use("/", indexRouter);
-app.use("/menus", [menusRouter, menuRankRputer]);
+app.use('/api/menus', menusRouter);
+
+app.get('/*', (req,res) =>{
+    res.sendFile(path.join(path.resolve() , 'public', 'index.html'));
+});
 
 export default app;
--- routes/menus.js
+++ routes/menus.js
@@ -58,4 +58,91 @@ router.get("/", async (req, res, next) => {
      });
 });
 
+// router.get("/", async (req, res, next) => {
+//      const menus = await prisma.menu.findMany({
+//           where: { id: true },
+//      });
+//      res.status(200).json({
+//           menus,
+//      });
+// });
+
+//????
+router.get("/:menuId", async (req, res, next) => {
+     const id = req.params.menuId;
+
+     console.log(id);
+
+     if (id === undefined || id === "") {
+          return res.status(200).json({ message: "메뉴 생성 페이지입니다." });
+     }
+
+     if (isNaN(+id)) {
+          return res.status(400).json({
+               error: "유효하지 않은 메뉴 ID입니다.",
+          });
+     }
+
+     const menu = await prisma.menu.findUnique({
+          where: { id: +id },
+     });
+
+     res.status(200).json({ menu });
+});
+
+// 메뉴  생성
+router.post("/", async (req, res, next) => {
+     const { name, type, temperature, price } = req.body;
+
+     const createMenu = await prisma.menu.create({
+          data: {
+               name: name,
+               type: type,
+               temperature: temperature,
+               price: +price,
+          },
+     });
+
+     res.status(201).json({
+          message: "메뉴 생성되었습니다.",
+          menu: {
+               createMenu,
+          },
+     });
+});
+
+// 메뉴 수정 하기
+router.put("/:menuId", async (req, res, next) => {
+     const id = req.params.menuId;
+     const { name, type, temperature, price } = req.body;
+
+     const updateMenu = await prisma.Menu.update({
+          where: { id: +id },
+          data: {
+               name: name,
+               type: type,
+               temperature: temperature,
+               price: +price,
+          },
+     });
+     res.status(200).json({
+          message: `메뉴 ${id} 수정되었습니다.`,
+          data: updateMenu,
+     });
+});
+
+//메뉴 삭제 하기
+router.delete("/:menuId", async (req, res, next) => {
+     const id = req.params.menuId;
+     const deleteMenu = await prisma.Menu.delete({
+          where: { id: +id },
+     });
+
+     console.log(deleteMenu);
+
+     res.status(200).json({
+          message: `메뉴 ${id} 삭제되었습니다.`,
+     });
+});
+
 export default router;
